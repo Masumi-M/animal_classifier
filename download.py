f1327f7212adce5ca29f77f0988307e05c8c6413
Masumi-M <masumi.developer@gmail.com>
2020-03-25 16:28:42 +0900

    Edit: download.py

--- download.py
+++ download.py
@@ -1,6 +1,39 @@
-from flickrapi import flickrapi
+from flickrapi import FlickrAPI
 from urllib.request import urlretrieve
 from pprint import pprint
 import os
 import time
 import sys
+
+path_api_id = './api_id.txt'
+path_api_secret = './api_secret.txt'
+
+f = open(path_api_id)
+api_id = f.read()
+f.close()
+
+f = open(path_api_secret)
+api_secret = f.read()
+f.close()
+
+print(api_id)
+print(api_secret)
+
+wait_time = 1
+
+animalname = sys.argv[1]
+savedir = "./" + animalname
+
+flickr = FlickrAPI(api_id, api_secret, format='parsed-json')
+
+result = flickr.photos.search(
+    text=animalname,
+    per_page=400,
+    media='photos',
+    sort='relevance',
+    safe_search=1,
+    extras='url_q, licence'
+)
+
+photos = result['photos']
+pprint(photos)
