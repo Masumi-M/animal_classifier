d7ad307221e8671cf74d5367287a4497f0e08cda
Masumi Morishige <masumi_morishige@Masumi-Mac-Pro-300.local>
2020-03-27 18:25:39 +0900

    Add: Loader

--- animal_cnn.py
+++ animal_cnn.py
@@ -1,4 +1,5 @@
 from keras.models import Sequential
+
 # from tensorflow.keras import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
 from keras.utils import np_utils
@@ -13,10 +14,9 @@ epoch_num = 10
 
 
 def main():
-    X_train, X_test, Y_train, Y_test = np.load(
-        "./animal.npy", allow_pickle=True)
+    X_train, X_test, Y_train, Y_test = np.load("./animal.npy", allow_pickle=True)
 
-    X_train = X_train.astype("float") / 256 # 要確認
+    X_train = X_train.astype("float") / 256  # 要確認
     X_test = X_test.astype("float") / 256
     Y_train = np_utils.to_categorical(Y_train, num_classes)
     Y_test = np_utils.to_categorical(Y_test, num_classes)
@@ -34,46 +34,52 @@ def main():
 def model_train(X_train, Y_train, X_test, Y_test):
     model = Sequential()
 
-    model.add(Conv2D(32, (3, 3,), padding='same',
-                     input_shape=X_train.shape[1:]))
-    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3,), padding="same", input_shape=X_train.shape[1:]))
+    model.add(Activation("relu"))
     model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('relu'))
+    model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
-    model.add(Conv2D(64, (3, 3,), padding='same'))
-    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3,), padding="same"))
+    model.add(Activation("relu"))
     model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('relu'))
+    model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
-    model.add(Flatten())    # Flat処理、一列にする
+    model.add(Flatten())  # Flat処理、一列にする
 
     model.add(Dense(512))  # 全結合層
-    model.add(Activation('relu'))
+    model.add(Activation("relu"))
     model.add(Dropout(0.5))
     model.add(Dense(3))
-    model.add(Activation('softmax'))
+    model.add(Activation("softmax"))
 
     opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)  # 最適化処理
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=opt, metrics=['accuracy'])  # 損失関数
+    model.compile(
+        loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"]
+    )  # 損失関数
 
     # hist = model.fit(X_train, Y_train, batch_size=32, epochs=epoch_num, validation_split=0.2)
-    hist = model.fit(X_train, Y_train, batch_size=32, epochs=epoch_num, validation_data=(X_test, Y_test))
+    hist = model.fit(
+        X_train,
+        Y_train,
+        batch_size=32,
+        epochs=epoch_num,
+        validation_data=(X_test, Y_test),
+    )
 
     print(hist.history)
-    model.save('./animal_cnn.h5')
+    model.save("./animal_cnn.h5")
 
     return model
 
 
 def model_eval(model, X, Y):
     scores = model.evaluate(X, Y, verbose=1)
-    print('Test Loss: ', scores[0])
-    print('Accuracy: ', scores[1])
+    print("Test Loss: ", scores[0])
+    print("Accuracy: ", scores[1])
 
 
 if __name__ == "__main__":
--- download.py
+++ download.py
@@ -5,11 +5,13 @@ from pprint import pprint
 import os
 import time
 import sys
+from tqdm import tqdm
 
 
 def main(animalname):
-    path_api_id = './api_id.txt'
-    path_api_secret = './api_secret.txt'
+    pprint("Download [" + animalname + "]")
+    path_api_id = "./api_id.txt"
+    path_api_secret = "./api_secret.txt"
 
     f = open(path_api_id)
     api_id = f.read()
@@ -19,38 +21,43 @@ def main(animalname):
     api_secret = f.read()
     f.close()
 
-    print(api_id)
-    print(api_secret)
-
     wait_time = 1
 
     savedir = "./" + animalname
     if not os.path.exists(savedir):
         os.mkdir(savedir)
+    photo_num = 400
 
-    flickr = FlickrAPI(api_id, api_secret, format='parsed-json')
+    flickr = FlickrAPI(api_id, api_secret, format="parsed-json")
 
     result = flickr.photos.search(
         text=animalname,
-        per_page=400,
-        media='photos',
-        sort='relevance',
+        per_page=photo_num,
+        media="photos",
+        sort="relevance",
         safe_search=1,
-        extras='url_q, licence'
+        extras="url_q, licence",
     )
 
-    photos = result['photos']
+    photos = result["photos"]
 
-    # pprint(photos)
+    pprint("  Fetch Completed.")
+    pprint("  Start Download.")
 
-    for i, photo in enumerate(photos['photo']):
-        url_q = photo['url_q']
-        filepath = savedir + '/' + photo['id'] + '.jpg'
+    start_time = time.time()
+    download_loader = tqdm(total=photo_num)
+    for i, photo in enumerate(photos["photo"]):
+        download_loader.update(1)
+        url_q = photo["url_q"]
+        filepath = savedir + "/" + photo["id"] + ".jpg"
         if os.path.exists(filepath):
             continue
         urlretrieve(url_q, filepath)
         time.sleep(wait_time)
 
+    download_loader.close()
+    pprint("Calc_time: [" + string(time.time() - start_time) + "]")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main(sys.argv[1])
