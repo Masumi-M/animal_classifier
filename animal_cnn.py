e75af10b6a73c73340bd48c937832843f8176e4e
Masumi.M <masumi.lacrosse@live.jp>
2020-03-29 15:59:48 +0900

    Fixed: traing_figure

--- animal_cnn.py
+++ animal_cnn.py
@@ -9,6 +9,7 @@ import pickle
 import os
 import time
 import h5py
+import matplotlib.pyplot as plt
 
 classes = ["dog", "cat", "turtle"]
 num_classes = len(classes)
@@ -46,10 +47,12 @@ def main():
     print("\n===== Model Evaluation =====")
     model_eval(model, X_test, Y_test)
 
+
 def model_train(X_train, Y_train, X_test, Y_test):
     model = Sequential()
 
-    model.add(Conv2D(32, (3, 3,), padding="same", input_shape=X_train.shape[1:]))
+    model.add(Conv2D(32, (3, 3,), padding="same",
+                     input_shape=X_train.shape[1:]))
     model.add(Activation("relu"))
     model.add(Conv2D(32, (3, 3)))
     model.add(Activation("relu"))
@@ -100,10 +103,55 @@ def model_eval(model, X, Y):
     print("Test Loss: ", scores[0])
     print("Test Accuracy: ", scores[1])
 
-    hist_file = open(database_path + "/history.pkl", "r")
+    hist_file = open(database_path + "/history.pkl", "rb")
     hist_data = pickle.load(hist_file)
     hist_file.close()
+
+    hist_visualize(hist_data)
+
+    return
+
+
+def hist_visualize(hist_data):
     print(hist_data)
+
+    acc = hist_data['accuracy']
+    loss = hist_data['loss']
+    val_acc = hist_data['val_accuracy']
+    val_loss = hist_data['val_loss']
+
+    epochs = range(len(acc))
+
+    fig = plt.figure(figsize=(7, 4))
+
+    # Accuracy
+    ax_acc = fig.add_subplot(1, 2, 1)
+    ax_acc.plot(epochs, acc, label='Training')
+    ax_acc.plot(epochs, val_acc, label='Validation')
+    ax_acc.set_xlabel('Epochs')
+    ax_acc.set_ylabel('Accuracy Score')
+    ax_acc.set_ylim([0, 1])
+    ax_acc.set_title('Accuracy')
+    ax_acc.legend(loc='best')
+    ax_acc.grid()
+
+    # Loss
+    ax_loss = fig.add_subplot(1, 2, 2)
+    ax_loss.plot(epochs, loss, label='Training')
+    ax_loss.plot(epochs, val_loss, label='Validation')
+    ax_loss.set_xlabel('Epochs')
+    ax_loss.set_ylabel('Loss Score')
+    ax_loss.set_ylim([0, 1])
+    ax_loss.set_title('Loss')
+    ax_loss.legend(loc='best')
+    ax_loss.grid()
+
+    # Save and Show
+    plt.subplots_adjust(left=0.125, right=0.9, bottom=0.15,
+                        top=0.9, wspace=0.3, hspace=0.2)
+    plt.savefig(database_path + '/train_result.png')
+    # plt.show()
+    return
 
 
 if __name__ == "__main__":
