18157a8bd9e64eb466959564f947edbf1d8af289
Masumi Morishige <masumi_morishige@Lunch-Box.local>
2020-04-28 23:20:27 +0900

    Add: notify and removed cache

---
+++ notify_line.py
@@ -0,0 +1,20 @@
+import requests
+import os
+import sys
+
+if os.path.exists('line_token.txt'):
+    file_token = open('line_token.txt')
+    line_notify_token = file_token.read()
+    file_token.close()
+
+    line_notify_api = "https://notify-api.line.me/api/notify"
+    if len(sys.argv) == 2:
+        message = '\nTractography Program Finished. \n 【' + sys.argv[1]  + '】'
+    else:
+        message = '\nTractography Program Finished.'
+
+    payload = {'message': message}
+    headers = {'Authorization': 'Bearer ' + line_notify_token}
+    line_notify = requests.post(line_notify_api, data=payload, headers=headers)
+else:
+    print("Please Setup Line Notify Token.")
