7abde7f09a5abced5e11176cd5259c4bf2e6f0ac
Masumi Morishige <masumi.developer@gmail.com>
2020-04-30 01:48:13 +0900

    Edit: image_num

--- parameter.py
+++ parameter.py
@@ -5,10 +5,10 @@ classes = ["dog", "crow"]
 num_classes = len(classes)
 image_size = 256
 input_image_num = 300
+val_data_num = 70
 epoch_num = 50
 kernel_size = 5
 lay1_width = 32
 lay2_width = 64
-val_data_num = 100
 opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)  # 最適化処理
 # opt = "adam"
