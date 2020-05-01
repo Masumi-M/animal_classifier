3afcfd664303008e36e0ecf7e57d420237f63600
Masumi Morishige <masumi.developer@gmail.com>
2020-05-01 10:22:16 +0900

    Edit: opt to adam

--- parameter.py
+++ parameter.py
@@ -10,11 +10,11 @@ cross_num = 4
 # input_image_num = 6
 # val_data_num = 3
 epoch_num = 50
-kernel_size = 5
+kernel_size = 3
 lay1_width = 32
 lay2_width = 64
-opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)  # 最適化処理
-# opt = "adam"
+# opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)  # 最適化処理
+opt = "adam"
 
 database_path = "./database/epoch_" + \
     str(epoch_num) + "_img" + str(image_size) + "_kernel" + str(kernel_size)
